70e4533b4076783f434d45b3e117018cd57ce44f
Endeavourarts <muhammadsk2610@gmail.com>
2022-08-05 20:21:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors :{
+        primary: 'rgb(192 38 211);',
+        dark: 'rgb(134 25 143);'
+      },
+    
+    screens: {
+      '2xl': '1320px',
+    },
+
+    }
+
+  },
+  plugins: [],
+}
